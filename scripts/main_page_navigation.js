3eeee89fcc4dd577f96ed8ed078920ca67e12652
Karl Portable <isordee@gmail.com>
2023-07-27 14:29:14 +0200

    Idle correct scroll working

--- scripts/main_page_navigation.js
+++ scripts/main_page_navigation.js
@@ -6,3 +6,8 @@ function correct_scroll(){
 		return ;
 	pf.scrollTo({left: (((scroll + ws / 2) / ws) >> 0) * ws, behavior: "smooth"});
 }
+let scrollDelay;
+function onScroll(e){
+	clearTimeout(scrollDelay);
+	scrollDelay = setTimeout(correct_scroll, 200);
+}
